3f0c14544d944b4881a645ba7e94450d5dea6bfa
VuHai <vhai66801@gmail.com>
2023-05-28 08:38:23 +0700

    project setting

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
+
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="Test-2-Firebase-esp8266")
+doc_ref = db.collection("Posts").document("Google")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
